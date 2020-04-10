5960eb7da0052a8fb9a16f6b368da59c7757a527
wcd0622 <wangchendi5@163.com>
2020-04-10 09:12:38 +0800

    雷哥提交

--- springmvc-demo/src/main/java/com/lagou/edu/controller/DemoController.java
+++ springmvc-demo/src/main/java/com/lagou/edu/controller/DemoController.java
@@ -1,7 +1,24 @@
 package com.lagou.edu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
 
 @Controller
+@RequestMapping("/demo")
 public class DemoController {
+    @RequestMapping("/handle01")
+    public ModelAndView handle01() {
+        Date date = new Date();//服务器时间
+        //返回服务器时间到前端页面
+        //封装了数据和页面信息的模型
+        ModelAndView modelAndView = new ModelAndView();
+        //addObject向请求域中request.setattribute("date",date);
+        modelAndView.addObject("date",date);
+        //视图信息（封装跳转的页面信息）
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
 }
